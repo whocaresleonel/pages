6666fce6ccd466db7127c2645755e1b13cdb7b2f
uniub <bruhgamerjobias@gmail.com>
2024-06-11 22:28:52 -0400

    Uniub v2.01
    Signed-off-by: uniub

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# **Universal UB 2.01!**
+**200+ proxies**
+
+<br><br><br><br>
+
 # **Universal UB 2.0!**
 **20+ More games**
 
--- js/homepage.js
+++ js/homepage.js
@@ -42,3 +42,17 @@ function changelog(){
         changelogopen = true
     }
 }
+
+function aboutblank(){
+    url = window.location
+    win = window.open();
+    win.document.body.style.margin = '0';
+    win.document.body.style.height = '100vh';
+    var iframe = win.document.createElement('iframe');
+    iframe.style.border = 'none';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.margin = '0';
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+}
