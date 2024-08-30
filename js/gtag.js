4e4fea3866788dbc542f1f806ee4c60443f9480b
uniub <bruhgamerjobias@gmail.com>
2024-08-30 15:08:24 -0400

    Added Gtag (Analytics)

---
+++ js/gtag.js
@@ -0,0 +1,6 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FG22JV74V5"></script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-FG22JV74V5');
